9472fd9a0c5306631d62f43a07fc2e0517f83cb6
Marcelo Gomez <marcelogomez321@gmail.com>
2015-03-27 17:57:27 -0600

    Tiny mistake in test_tokens.py corrected

--- test_tokens.py
+++ test_tokens.py
@@ -1,10 +1,9 @@
 __author__ = 'mng687'
 import requests
 
-payoad = {
+payload = {
     'grant_type': 'password',
     'username': 'pedro',
     'password': 'pedro',
-
 }
-print requests.post('http://rPGzzF1xtW4tBd5yG8tzJCZEncEK2e9wpewe4lNe:kjLlRgWc5lNIml00zjrxmssUvUeEAe4Rv5P3vI6aBG0DKVV8epU8Fxfo2mclYKVA03NahzqPdTZX0owOfotWckDm6UX6ub3KuHqfcNWlcvGAppJLZyiHz3xvKeYTrgWt@127.0.0.1:8000/o/token/').text
+print requests.post('http://rPGzzF1xtW4tBd5yG8tzJCZEncEK2e9wpewe4lNe:kjLlRgWc5lNIml00zjrxmssUvUeEAe4Rv5P3vI6aBG0DKVV8epU8Fxfo2mclYKVA03NahzqPdTZX0owOfotWckDm6UX6ub3KuHqfcNWlcvGAppJLZyiHz3xvKeYTrgWt@localhost:8000/o/token/', payload).text
